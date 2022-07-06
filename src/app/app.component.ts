d71cddf5f1fb38a5dec2004c969fb21c90f716b4
Saif <saifrahman169@gmail.com>
2022-07-06 19:38:56 +0530

    Created List and added CustomerDetailButton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {CustData} from './CustomerDataInterface'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'customer-details';
+  displayedColumns: string[] = ['name', 'email', 'phone', 'country','action'];
+
+  todoValue:string="";
+    tag:string="";
+    list: CustData[]=[];
+    /*list: CustData[]=[{
+      name:"Demo",
+      email:"demo@example.com",
+      phone:"9999999999",
+      country:"Demo Address"
+    }];*/
+
+
+
+    
+  constructor(private http: HttpClient){}
+  ngOnInit(){
+    this.http.get<any>('https://randomuser.me/api/').subscribe(data => {
+
+    if(data.info && data.info.results>0){
+      data.results.forEach((element: { name: { first: any; }; email: any; phone: any; location: { country: any; }; }) => {
+        this.list.push({name:element.name.first,email:element.email,phone:element.phone,country:element.location.country})
+      });
+      console.log("created data",this.list);
+    }
+      console.log(data);
+  })    
+  }
 }
