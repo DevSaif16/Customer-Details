a7af2653f35c032104ddfe5d4930b4f766872d11
Saif <saifrahman169@gmail.com>
2022-07-06 19:46:52 +0530

    Added Customer Data Interface File

---
+++ src/app/CustomerDataInterface.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@angular/core";
+
+import { Observable } from "rxjs";
+
+export interface CustData{
+    name:any,
+    email:any,
+    phone:any,
+    country:any
+}
+
+
